1c0ff840ab01fb1a7f58b5112be9e3389ed463b6
Quang Vu <vhquang@gmail.com>
2018-07-07 17:13:17 -0700

    Refactor variables name

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,19 +1,20 @@
 import os
 
-#Import the necessary methods from tweepy library
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API
-access_token        = os.environ['ACCESS_TOKEN']
-access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
-consumer_key        = os.environ['CONSUMER_KEY']
-consumer_secret     = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 
 
-#This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
+    """
+    Basic listener that output tweet to stdout
+    """
 
     def on_data(self, data):
         print(data)
@@ -24,14 +25,11 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
-
-    #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
     stream.filter(track=[
         # 'fifa',
         'worldcup',
